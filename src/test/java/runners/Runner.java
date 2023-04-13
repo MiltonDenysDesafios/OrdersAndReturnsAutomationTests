0ec7f15a1fe7e597b77a77c988dd0ebbb9f8d8e2
Milton Denys <milton.silva17@fatec.sp.gov.br>
2023-04-13 20:01:48 -0300

    tags = "@OrdersAndReturns"

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/java/features/",
         glue = "",
-        tags = "@OrdersAndReturns_ID17"
+        tags = "@OrdersAndReturns"
 )
 public class Runner {
 }
